5fae2da2cfd9c86964ec76019f6ec419c0b0659e
Lydon Camilleri <69397229+Marsimio@users.noreply.github.com>
2023-11-13 17:19:53 +0100

    Flame Tongue Update

--- src/main/java/net/marsim/zejzamod/item/custom/FlameTongueItem.java
+++ src/main/java/net/marsim/zejzamod/item/custom/FlameTongueItem.java
@@ -1,6 +1,10 @@
 package net.marsim.zejzamod.item.custom;
 
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.*;
 import net.minecraft.world.level.Explosion;
 import net.minecraft.world.level.Level;
@@ -17,13 +21,51 @@ public class FlameTongueItem extends SwordItem {
 
             world.explode(
                     attacker,
-                    target.getX(),
-                    target.getY(),
-                    target.getZ(),
+                    attacker.getX(),
+                    attacker.getY(),
+                    attacker.getZ(),
                     3.0F,
                     Level.ExplosionInteraction.NONE
             );
         }
         return super.hurtEnemy(stack, target, attacker);
     }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level world, Player player, InteractionHand hand) {
+        ItemStack itemstack = player.getItemInHand(hand);
+        player.startUsingItem(hand);
+        return new InteractionResultHolder<>(InteractionResult.SUCCESS, itemstack);
+    }
+
+    @Override
+    public ItemStack finishUsingItem(ItemStack stack, Level world, LivingEntity entityLiving) {
+        // Explosion logic goes here
+        if (!world.isClientSide && entityLiving instanceof Player) {
+            world.explode(
+                    null,
+                    entityLiving.getX(),
+                    entityLiving.getY(),
+                    entityLiving.getZ(),
+                    3.0F,
+                    Level.ExplosionInteraction.NONE
+            );
+        }
+
+        // Optional: Implement item durability reduction or other effects
+
+        return super.finishUsingItem(stack, world, entityLiving);
+    }
+
+    @Override
+    public int getUseDuration(ItemStack stack) {
+        return 32; // Duration in ticks (e.g., 32 ticks for quick eat action)
+    }
+
+    @Override
+    public UseAnim getUseAnimation(ItemStack stack) {
+        return UseAnim.EAT; // Eating animation
+    }
+
+
 }
